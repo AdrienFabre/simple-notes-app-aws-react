2e15b856216ac73e4808d38aef59467c0669a378
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-07-29 14:55:53 +0100

    Configure AWS Amplify

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "notes-app-uploads-af"
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://u9pkdb1nab.execute-api.eu-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_Lhl4ODIiP",
+    APP_CLIENT_ID: "t4f0sq041j0vfpsu3rqrr35nr",
+    IDENTITY_POOL_ID: "eu-west-2:00e27203-f1bf-43d0-9f69-0cce265230e0"
+  }
+};
